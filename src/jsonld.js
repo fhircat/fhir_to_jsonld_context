7b87146f3dcb82164f9e500518b1d24127981947
jiaola <djiao@jhu.edu>
2020-01-09 13:25:44 -0500

    Add general jsonld command line tool

---
+++ src/jsonld.js
@@ -0,0 +1,30 @@
+import jsonld from 'jsonld'
+import fs from 'fs'
+import optimist from 'optimist'
+
+const opts = optimist.usage('$0: A command line JSONLD script')
+    .describe('i', 'JSON file to process')
+    .alias('i', 'input')
+    .describe('c', 'Command: expand, frame or compact')
+    .alias('c', 'command')
+    .describe('x', 'JSONLD Context')
+    .alias('x', 'context')
+    .describe('f', 'frame file')
+    .alias('f', 'frame')
+    .demand(['c', 'i'])
+
+const argv = opts.argv
+
+const content = fs.readFileSync(argv.input).toString()
+let options = {}
+if (argv.context) {
+    options['expandContext'] = argv.context
+} else if (argv.frame) {
+    options = JSON.parse(fs.readFileSync(argv.frame).toString())
+}
+const command = argv.command
+if (command in jsonld && typeof jsonld[command] === 'function') {
+    jsonld[command](JSON.parse(content), options).then(val => {
+        console.log(JSON.stringify(val, null, 2))
+    })
+}
